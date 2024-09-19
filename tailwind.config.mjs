e80c8a1b16e4fc39147faa93576949c17e769332
Ujjwal Talwar <mail2ujjwal98@gmail.com>
2024-09-19 18:55:00 +0530

    added header and basic styles

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#373A40",
+        "dark-transparent": "#111111BF",
+        light: "#EEEEEE",
+        "light-transparent": "#FFFFFF5F",
+        orange: "#DC5F00",
+        gray: "#686D76",
+        "dark-gray": "#1A1A1A",
+      },
+      fontFamily: {
+        montserrat: ["Montsserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
